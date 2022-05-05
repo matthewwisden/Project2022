1bb912d7590f86d6ae725d0d73fac9d3934c2e8c
matthewwisden <74996765+matthewwisden@users.noreply.github.com>
2022-05-05 20:27:21 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,62 +1,120 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
-net = cv2.dnn.readNet('yolov3_training_last.weights', 'yolov3_testing.cfg')
+# Distance variables
+KNOWN_DISTANCE = 114.3  # CM
+PLAYER_WIDTH = 27  # CM
+BALL_WIDTH = 10  # CM
+GOAL_WIDTH = 50  # CM
+# Object detection variables
+CONFIDENCE_THRESHOLD = 0.4
+NMS_THRESHOLD = 0.3
 
-classes = []
+# colors for object detected
+COLORS = [(255, 0, 0), (255, 0, 255), (0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+# defining fonts
+FONTS = cv.FONT_HERSHEY_COMPLEX
+
+# getting class names from classes.txt file
+class_names = []
 with open("classes.txt", "r") as f:
-    classes = f.read().splitlines()
+    class_names = [cname.strip() for cname in f.readlines()]
+#  setttng up opencv net
+yoloNet = cv.dnn.readNet('yolov4_training_last.weights', 'yolov4_testing.cfg')
 
-cap = cv2.VideoCapture('image.jpg')
-font = cv2.FONT_HERSHEY_PLAIN
-colors = np.random.uniform(0, 255, size=(100, 3))
+yoloNet.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
+yoloNet.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA_FP16)
 
+model = cv.dnn_DetectionModel(yoloNet)
+model.setInputParams(size=(416, 416), scale=1 / 255, swapRB=True)
+
+
+
+def object_detector(image):
+    classes, scores, boxes = model.detect(image, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
+
+    data_list = []
+    for (classid, score, box) in zip(classes, scores, boxes):
+
+        color = COLORS[int(classid) % len(COLORS)]
+
+        label = "%s : %f" % (class_names[classid[0]], score)
+
+        # draw rectangle on and label on object
+        cv.rectangle(image, box, color, 2)
+        cv.putText(image, label, (box[0], box[1] - 14), FONTS, 0.5, color, 2)
+
+
+        if classid == 0:  # Player class id
+            data_list.append([class_names[classid[0]], box[2], (box[0], box[1] - 2)])
+        elif classid == 1:  # Ball
+            data_list.append([class_names[classid[0]], box[2], (box[0], box[1] - 2)])
+        elif classid == 2:  # car
+            data_list.append([class_names[classid[0]], box[2], (box[0], box[1] - 2)])
+    return data_list
+
+
+def focal_length_finder(measured_distance, real_width, width_in_rf):
+    focal_length = (width_in_rf * measured_distance) / real_width
+
+    return focal_length
+
+
+# distance finder function
+def distance_finder(focal_length, real_object_width, width_in_frame):
+    distance = (real_object_width * focal_length) / width_in_frame
+    return distance
+
+
+# reading the reference image from dir
+ref_player = cv.imread('player.jpg')
+ref_ball = cv.imread('ball.jpg')
+ref_goal = cv.imread('goal.jpg')
+
+##goal_data = object_detector(ref_goal)
+##goal_width_in_rf = goal_data[2][1]
+player_data = object_detector(ref_player)
+player_width_in_rf = player_data[0][1]
+
+ball_data = object_detector(ref_ball)
+ball_width_in_rf = ball_data[1][1]
+
+goal_data = object_detector(ref_goal)
+goal_width_in_rf = goal_data[2][1]
+
+
+print(f"player width in pixels : {player_width_in_rf} ball width in pixel: {ball_width_in_rf} ball width in pixel: {goal_width_in_rf}")
+
+# finding focal length
+focal_player = focal_length_finder(KNOWN_DISTANCE, PLAYER_WIDTH, player_width_in_rf)
+
+focal_ball = focal_length_finder(KNOWN_DISTANCE, BALL_WIDTH, ball_width_in_rf)
+
+focal_goal = focal_length_finder(KNOWN_DISTANCE, GOAL_WIDTH, goal_width_in_rf)
+cap = cv.VideoCapture(0)
 while True:
-    _, img = cap.read()
-    height, width, _ = img.shape
+    ret, frame = cap.read()
 
-    blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
-    net.setInput(blob)
-    output_layers_names = net.getUnconnectedOutLayersNames()
-    layerOutputs = net.forward(output_layers_names)
+    data = object_detector(frame)
+    for d in data:
+        if d[0] == 'player':
+            distance = distance_finder(focal_player, PLAYER_WIDTH, d[1])
+            x, y = d[2]
+        elif d[0] == 'ball':
+            distance = distance_finder(focal_ball, BALL_WIDTH, d[1])
+            x, y = d[2]
+        elif d[0] == 'goal':
+            distance = distance_finder(focal_goal, GOAL_WIDTH, d[1])
+            x, y = d[2]
+        cv.rectangle(frame, (x, y - 3), (x + 150, y + 23), BLACK, -1)
+        cv.putText(frame, f'Dis: {round(distance, 2)} inch', (x + 5, y + 13), FONTS, 0.48, GREEN, 2)
 
-    boxes = []
-    confidences = []
-    class_ids = []
+    cv.imshow('frame', frame)
 
-    for output in layerOutputs:
-        for detection in output:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > 0.2:
-                center_x = int(detection[0]*width)
-                center_y = int(detection[1]*height)
-                w = int(detection[2]*width)
-                h = int(detection[3]*height)
-
-                x = int(center_x - w/2)
-                y = int(center_y - h/2)
-
-                boxes.append([x, y, w, h])
-                confidences.append((float(confidence)))
-                class_ids.append(class_id)
-
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.2, 0.4)
-
-    if len(indexes) > 0:
-        for i in indexes.flatten():
-            x, y, w, h = boxes[i]
-            label = str(classes[class_ids[i]])
-            confidence = str(round(confidences[i], 2))
-            color = colors[i]
-            cv2.rectangle(img, (x, y), (x+w, y+h), color, 2)
-            cv2.putText(img, label + " " + confidence, (x, y+20), font, 2, (255, 255, 255), 2)
-
-    cv2.imshow('Image', img)
-    key = cv2.waitKey(0)
-    # if key == 27:
-        #break
-
+    key = cv.waitKey(1)
+    if key == ord('q'):
+        break
+cv.destroyAllWindows()
 cap.release()
-cv2.destroyAllWindows()
